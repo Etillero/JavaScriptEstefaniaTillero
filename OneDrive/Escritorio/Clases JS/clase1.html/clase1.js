b47fafe4b4ccd1da364f64d2739cf0ed17136167
Estefania Tillero <estefaniatillero@gmail.com>
2021-10-29 22:08:52 -0300

    entrega primer desafio js Estefania Tillero

---
+++ OneDrive/Escritorio/Clases JS/clase1.html/clase1.js
@@ -0,0 +1,16 @@
+
+let nombredeUsuario = prompt("Ingrese nombre");
+ console.log("Hola " + nombredeUsuario + " bienvenido a mi entrega");
+
+ let nu1 = 30;
+ let nu2 = parseInt(prompt("Ingresa un numero"));
+ 
+ let suma = nu1 + nu2;
+ console.log(suma);
+
+ let nombremascota = prompt("Ingresa el nombre de tu mascota");
+ let juguete = prompt("Indica el juguete favorito de tu mascota");
+
+ let resultado = "El juguete favorito de " + nombremascota + " es: " + juguete;
+
+ alert(resultado);
